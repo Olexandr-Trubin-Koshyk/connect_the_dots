e2eb79f52402d9bf69b6c7258f8fc0b9a4f047db
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-05-12 13:02:44 +0300

    integrate linters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,27 +1,32 @@
-'use strict';
-import { GameScene } from "./GameScene";
+import Phaser from 'phaser';
+// eslint-disable-next-line import/no-cycle
+import { GameScene } from './GameScene';
 
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  dotSize: 140,
-  dotColors: [
-    0xffff00, 
-    0x0000ff, 
-    0x00ff00, 
-    0xff0000, 
-    0xff00ff
-  ],
-  rows: 6,
-  cols: 6,
-  scale: {
+// eslint-disable-next-line import/prefer-default-export
+export const config = {};
+
+if (GameScene) {
+  // eslint-disable-next-line import/prefer-default-export
+  config.type = Phaser.AUTO;
+  config.width = 800;
+  config.height = 600;
+  config.dotSize = 140;
+  config.dotColors = [
+    0xffff00,
+    0x0000ff,
+    0x00ff00,
+    0xff0000,
+    0xff00ff,
+  ];
+  config.rows = 6;
+  config.cols = 6;
+  config.scale = {
     mode: Phaser.Scale.CENTER_BOTH,
     width: 2560,
     height: 1440,
-  },
-  scene: new GameScene(),
-};
+  };
+  config.scene = new GameScene();
 
-const game = new Phaser.Game(config);
-
+  // eslint-disable-next-line no-unused-vars
+  const game = new Phaser.Game(config);
+}
