b6a300a4161c6038c98189a95b51245507c03ab8
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-05-20 13:09:01 +0300

    Added dots fall animation

--- src/scripts/Dot.js
+++ src/scripts/Dot.js
@@ -5,7 +5,7 @@ import { config } from './main';
 
 export class Dot extends Phaser.GameObjects.Sprite {
   constructor(scene, color, position) {
-    super(scene, position.x, position.y, 'dot');
+    super(scene, position.x, -position.y, 'dot');
 
     this.position = {
       x: position.x,
@@ -17,6 +17,17 @@ export class Dot extends Phaser.GameObjects.Sprite {
     this.tint = color;
     this.scene.add.existing(this);
     this.setInteractive();
+  }
+
+  move() {
+    this.scene.tweens.add({
+      targets: this,
+      x: this.position.x,
+      y: this.position.y,
+      delay: ((config.scale.height - this.position.y) / config.dotSize) * 100,
+      ease: 'Linear',
+      duration: 300,
+    })
   }
 
   pickDot(boolean, oriental, move) {
